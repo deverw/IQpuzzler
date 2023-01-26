d6d6707ba84ecd619203a9a6bafea4ff835fed87
deverw <34944376+deverw@users.noreply.github.com>
2023-01-26 20:38:50 +0100

    Add files via upload

--- src/knuth_rect.cpp
+++ src/knuth_rect.cpp
@@ -1,74 +1,205 @@
 // Finds all solutions for IQ Puzzler 5x11 board by solving the exact coverage problem with Knuth's algorithm X.
 // Part definitions and orientations are read from text file "orig.2di"
-// or from filename.3di, where filename is defined in command line argument.
+// or from filename.2di, where filename is defined in command line argument.
 //
 // Written by Stefan Abendroth (sab@ab-solut.com)
-// Last update: 01/21/2023
+// Last update: 01/26/2023
 
-#include "../include/IQpuzzler_read_input.hpp"     // read part shapes and orientations from input file
+#include "IQpuzzler_read_input.hpp"     // read part shapes and orientations from input file
 
-#define BOARD_ROWS 5
-#define BOARD_COLUMNS 11
+constexpr auto BOARD_ROWS = 5;
+constexpr auto BOARD_COLUMNS = 11;
+constexpr auto PARTCOUNT = 12;
 
-// Part shapes incl. all possible orientations
-vector <vector <vector <vector <uint8_t> > > > part = {{{{}}}};
-uint8_t partcount;
+vector <vector <uint8_t> > X;
+vector <size_t> solution;
+vector <vector <size_t> > all_solutions;
 
-vector <vector <uint8_t> > X = {{}};
-
-using namespace std;
-
-void prepareX()
+/*
+void show_solution()
 {
-    // Prepare matrix for algorithm X.
-    // Each column represents a single part with a possible orientation and position on the board.
-    // Algorithm X will identify those sets of columns that can be combined to a completely filled column (=solution of exact coverage problem).
+    cout << "Solution " << to_string(all_solutions.size()) << endl;
+    for (size_t i = 0; i < solution.size(); i++)
+    {
+        for (size_t j=0; j<X[0].size();j++)
+            cout << to_string(X[solution[i]][j]) << " ";
+        cout << endl;
+    }
+    cout << endl;
+}
+*/
 
-    vector <uint8_t> Xcol(partcount+BOARD_ROWS*BOARD_COLUMNS);
-    uint8_t pos;
+void show_solution()
+{
+    vector <uint8_t> board(X[0].size(),0);
+    for (size_t i = 0; i < solution.size(); i++)
+        for (size_t j=0; j < board.size(); j++)
+            board[j] += X[solution[i]][j];
+    cout << "Solution " << to_string(all_solutions.size()) << ":";
+    for (size_t j = 0; j < PARTCOUNT; j++)
+        cout << " " << to_string(board[j]);
+    cout << endl;
+    for (size_t i = 0; i < BOARD_ROWS; i++)
+    {
+        for (size_t j = 0; j < BOARD_COLUMNS; j++)
+            cout << to_string(board[PARTCOUNT+j*BOARD_ROWS+i]) << " ";
+        cout << endl;
+    }
+    cout << endl;
+}
+
+/*
+void show_X()
+{
+    vector <uint8_t> board(X[0].size(), 0);
+    for (size_t n = 0; n < X.size(); n++)
+    {
+        cout << n << ": ";
+        for (size_t j = 0; j < PARTCOUNT; j++)
+            cout << " " << to_string(X[n][j]);
+        cout << endl;
+        for (size_t i = 0; i < BOARD_ROWS; i++)
+        {
+            for (size_t j = 0; j < BOARD_COLUMNS; j++)
+                cout << (char)(X[n][PARTCOUNT + j * BOARD_ROWS + i]+65) << " ";
+            cout << endl;
+        }
+    }
+}
+*/
+
+void prepareX(vector <vector <vector <vector <uint8_t> > > > part)
+// Prepare matrix for Knuth's algorithm X.
+// Each row of X represents a single part with a possible orientation and position on the board.
+// Algorithm X will identify those sets of rows that can be combined to a completely filled column (=solution of exact coverage problem).
+{
+    size_t xpos, ypos, partcount = part.size();
+    vector <uint8_t> Xrow(partcount + BOARD_ROWS * BOARD_COLUMNS, 0);
     bool fit;
 
-    for (uint8_t part_number=1; part_number<=partcount; part_number++)
+    for (size_t part_number = 0; part_number < partcount; part_number++)
     {
-        fill(Xcol.begin(),Xcol.end(),0);
-        Xcol[part_number-1]=1;
-        for (uint8_t orientation=0; orientation<part[part_number].size(); orientation++)
-            for (uint8_t row=0; row<BOARD_ROWS; row++)
-                for (uint8_t col=0; col<BOARD_COLUMNS; col++)
+        fill(Xrow.begin(), Xrow.end(), 0);
+        Xrow[part_number] = 1;
+        for (size_t orientation = 0; orientation < part[part_number].size(); orientation++)
+            for (size_t row = 0; row < BOARD_ROWS; row++)
+                for (size_t col = 0; col < BOARD_COLUMNS; col++)
                 {
-                    fit=true;
-                    fill(Xcol.begin()+partcount,Xcol.end(),0);
-                    for (uint8_t dot=0; dot<part[part_number][orientation].size(); dot++)
+                    fit = true;
+                    for (size_t dot = 0; dot < part[part_number][orientation].size(); dot++)
                     {
-                        pos = row+part[part_number][orientation][dot][1] + (col+part[part_number][orientation][dot][0])*BOARD_ROWS;
-                        if (pos < BOARD_ROWS*BOARD_COLUMNS)
-                            Xcol[partcount+pos] = part_number;
+                        xpos = col + part[part_number][orientation][dot][0];
+                        ypos = row + part[part_number][orientation][dot][1];
+                        if (xpos < BOARD_COLUMNS && ypos < BOARD_ROWS)
+                            Xrow[partcount + xpos*BOARD_ROWS + ypos] = part_number+1;
                         else
-                            fit=false;
+                            fit = false;
                     }
                     if (fit)
-                        X.push_back(Xcol);
+                        X.push_back(Xrow);
+                    fill(Xrow.begin() + partcount, Xrow.end(), 0);
                 }
     }
 }
 
-int main(int argc, char *argv[])
+void knuth(vector < vector <uint8_t> > A , vector <size_t> p)
+// Implementation of Knuth's algorithm X to solve exact coverage problem, as published in
+// https://www.ocf.berkeley.edu/~jchu/publicportal/sudoku/0011047.pdf
+// Arguments:
+// Matrix 'A' stores remaining coverage matrix. Each row represents a way to place a part on the board.
+// Vector 'p' stores row numbers of original coverage matrix X.
+{
+    if (A.size())
+    {
+        // choose column with minimum number of entries
+        // (this will minimize the number of recursions)
+        vector <size_t> entries(A[0].size(), 0);
+        uint32_t min_entries = UINT32_MAX;
+        size_t min_column = 0;
+        for (size_t col = 0; col < A[0].size(); col++)
+        {
+            for (size_t row = 0; row < A.size(); row++)
+                entries[col] += A[row][col] > 0;
+            if (entries[col] < min_entries)
+            {
+                min_entries = entries[col];
+                min_column = col;
+            }
+        }
+        if (min_entries)
+            // for each row such that A[row][min_column]>0
+            for (size_t row=0; row<A.size(); row++)
+                if (A[row][min_column])
+                {
+                    // include row in partial solution
+                    solution.push_back(p[row]);
+                    // show_solution();
+                    vector < vector <uint8_t> > Ar = A;
+                    vector <size_t> pr = p, del_rows(A.size(), 0);
+                    // for each j such that A[row][j]>0
+                    for (size_t j = A[row].size(); j > 0; j--)  // backwards to avoid indexing errors
+                        if (A[row][j-1])
+                        {
+                            // delete column j from A
+                            for (size_t r = Ar.size(); r > 0 ; r--)
+                                Ar[r-1].erase(Ar[r-1].begin() + j-1);
+                            // for each i such that A[i][j]>0
+                            for (size_t i = A.size(); i > 0; i--)  // backwards to avoid indexing errors
+                                if (A[i-1][j-1])
+                                    // mark row for deletion
+                                    del_rows[i - 1] = 1;
+                        }
+                    // delete rows from A (and p)
+                    for (size_t r = Ar.size(); r > 0 ; r--)
+                        if (del_rows[r-1])
+                        {
+                            Ar.erase(Ar.begin() + r-1);
+                            pr.erase(pr.begin() + r-1);
+                        }
+                    // repeat algorithm recursively on the reduced matrix A (and p)
+                    knuth(Ar, pr);
+                    solution.pop_back();
+                }      
+    }
+    // if A is empty, a solution is found
+    else if (solution.size()==PARTCOUNT)
+    {
+        all_solutions.push_back(solution);
+        show_solution();
+    }
+}
+
+uint32_t main(int argc, char* argv[])
 {
     string inputfile, outputfile;
 
-    if (argc>1)
+    if (argc > 1)
     {
-        inputfile=argv[1];
-        inputfile+=".2di";
-        outputfile=argv[1];
-        outputfile+="_rect.2do";
+        inputfile = argv[1];
+        inputfile += ".2di";
+        outputfile = argv[1];
+        outputfile += "_rect.2do";
     }
     else
     {
-        inputfile="orig.2di";
-        outputfile="orig_rect.2do";
+        inputfile = "orig.2di";
+        outputfile = "orig_rect.2do";
     }
 
-    partcount=read_input(inputfile, part);
-    prepareX();
+    prepareX(read_input(inputfile));
+    /*
+    X = { {0,0,0,0,1,1,1,1},{1,1,1,1,0,0,0,0},{0,0,2,2,0,0,2,2},{2,2,0,0,2,2,0,0},{0,3,0,3,0,3,0,3},{3,0,3,0,3,0,3,0} };
+    X = { {1,0,0,1,1,1,1,0,0,0},{1,0,0,0,1,1,1,1,0,0},{1,0,0,0,0,1,1,1,1,0},{1,0,0,0,0,0,1,1,1,1},
+         {0,1,0,2,2,0,0,0,0,0},{0,1,0,0,0,2,2,0,0,0},{0,1,0,0,0,0,0,2,2,0},{0,1,0,0,0,0,0,0,2,2},
+         {0,0,1,3,0,0,0,0,0,0},{0,0,1,0,0,3,0,0,0,0},{0,0,1,0,0,0,0,3,0,0},{0,0,1,0,0,0,0,0,0,3} };
+
+    show_X();
+    */
+    
+    vector <size_t> p(X.size());
+    for (size_t i = 0; i < p.size(); i++)
+        p[i] = i;
+    knuth(X, p);
+
+    return(all_solutions.size());
 }
